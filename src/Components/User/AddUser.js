cd7ee5d62db2498480955e9d2e9bcb2b2dc6b255
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-11 16:48:19 +0530

    added Card component

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,7 +1,8 @@
 import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
 const AddUser = () => {
   return (
-    <div className={`${classes.form_outer}`}>
+    <Card className={`${classes.form_outer}`}>
       <form>
         <label htmlFor="username">User Name</label>
         <input id="username" type="text"></input>
@@ -9,7 +10,7 @@ const AddUser = () => {
         <input id="age" type="number"></input>
         <button>Add User</button>
       </form>
-    </div>
+    </Card>
   );
 };
 
